bb60d1b82c5304c861c9a2dc4f2749e031d2b86d
aggroenewoud <aggroenewoud@gmail.com>
2024-06-06 21:03:55 +0200

    Small fix to make the routing work

--- mendel-dashboard/src/environments/environment.development.ts
+++ mendel-dashboard/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = { // Fill your own data in here
     production: false,
-    apiBaseUrl: "",
-    apiContract: "",
-    apiKey: ""
+    apiBaseUrl: "http://localhost:8000/api/contracten",
+    apiContract: "4",
+    apiKey: "rv4CW8jdoBwihT8sv2CNxRy0tsOm5eGf"
 };
