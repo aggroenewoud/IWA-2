709a7f3e02233895eda4d501e8aacd8ddf2741dc
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-06-06 20:10:31 +0200

    dsadsadsa

--- mendel-dashboard/src/app/home/home.component.ts
+++ mendel-dashboard/src/app/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { NewsInterface } from '../news-interface';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  news: NewsInterface[] = [];
 
+  constructor(private newsService: NewsService) { }
+
+  ngOnInit(): void {
+    this.news = this.newsService.getAllNews();
+  }
 }
