31c30c7b4b46e43f0b96db2426e70a5fef53e33d
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-06-06 21:15:47 +0200

    made cards on the home screen

--- mendel-dashboard/src/app/home/home.component.ts
+++ mendel-dashboard/src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { NewsComponent } from '../news/news.component';
 import { NewsInterface } from '../news-interface';
 import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [NewsComponent,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
