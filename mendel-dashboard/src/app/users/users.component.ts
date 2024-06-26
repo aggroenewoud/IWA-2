84e150f3469f0e066a17bc60ccd5bf0312c64a97
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-06-26 13:10:41 +0200

    form for add users made

--- mendel-dashboard/src/app/users/users.component.ts
+++ mendel-dashboard/src/app/users/users.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
-  selector: 'app-users',
-  standalone: true,
-  imports: [],
+  selector: 'app-user-form',
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrls: ['./users.component.css'],
+  imports: [ReactiveFormsModule], // Import ReactiveFormsModule here
+  standalone: true
 })
 export class UsersComponent {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.userForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      role: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      this.userService.createUser(this.userForm.value).subscribe(response => {
+        console.log('User created:', response);
+        this.userForm.reset();
+      });
+    }
+  }
 }
