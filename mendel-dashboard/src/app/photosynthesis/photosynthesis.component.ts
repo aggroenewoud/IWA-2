05d3003efbbe389a1a0795420aa55288e798b66c
The-Big-Fella <jornderoos@hotmail.com>
2024-06-26 19:40:33 +0200

    Added tables to wind & photosynthesis

--- mendel-dashboard/src/app/photosynthesis/photosynthesis.component.ts
+++ mendel-dashboard/src/app/photosynthesis/photosynthesis.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-photosynthesis',
   standalone: true,
-  imports: [
-    RouterOutlet
-  ],
+    imports: [
+        RouterOutlet,
+        NgForOf
+    ],
   templateUrl: './photosynthesis.component.html',
   styleUrl: './photosynthesis.component.css'
 })
