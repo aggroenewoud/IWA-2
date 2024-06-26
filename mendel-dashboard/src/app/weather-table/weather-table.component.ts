669a0008bdcd1f6a4351c2432ffd0a7156afa9ea
martprenger <mart.prenger@gmail.com>
2024-06-26 21:32:31 +0200

    added csv download

--- mendel-dashboard/src/app/weather-table/weather-table.component.ts
+++ mendel-dashboard/src/app/weather-table/weather-table.component.ts
@@ -3,6 +3,7 @@ import {StationData} from "../structs/stationData";
 import {WeatherDataService} from "../services/weather-data.service";
 import {DatePipe, DecimalPipe, NgForOf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import * as Papa from 'papaparse';
 
 @Component({
   selector: 'app-weather-table',
@@ -11,7 +12,7 @@ import {FormsModule} from "@angular/forms";
     DatePipe,
     NgForOf,
     DecimalPipe,
-    FormsModule
+    FormsModule,
   ],
   templateUrl: './weather-table.component.html',
   styleUrl: './weather-table.component.css'
@@ -56,5 +57,48 @@ export class WeatherTableComponent {
     newDate.setSeconds(this.startDate.getSeconds());
     newDate.setMilliseconds(this.startDate.getMilliseconds());
     this.endDate = newDate;
+  }
+
+  downloadCSV() {
+    // Prepare the data
+    const data = this.weatherData.flatMap(station =>
+      station.hourlyData.map(hourData => ({
+        StationID: station.StationId,
+        Time: hourData.date,
+        CLDC: hourData.CLDC,
+        DEWP: hourData.DEWP,
+        FRSHTT: hourData.FRSHTT,
+        PRCP: hourData.PRCP,
+        SLP: hourData.SLP,
+        SNDP: hourData.SNDP,
+        STP: hourData.STP,
+        TEMP: hourData.TEMP,
+        VISIB: hourData.VISIB,
+        WDSP: hourData.WDSP,
+        WNDDIR: hourData.WNDDIR
+      }))
+    );
+
+    // Convert the data to CSV
+    const csv = Papa.unparse(data);
+
+    // Create a blob from the CSV string
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+
+    // Create a link element
+    const link = document.createElement("a");
+
+    if (link.download !== undefined) {
+      // Create a URL to the blob
+      const url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", "weather_data.csv");
+      link.style.visibility = 'hidden';
+
+      // Add the link to the DOM and simulate a click
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   }
 }
