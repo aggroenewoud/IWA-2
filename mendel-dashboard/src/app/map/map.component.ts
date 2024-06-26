aaad975e31d52f5b9194e622775074234b07c333
martprenger <mart.prenger@gmail.com>
2024-06-26 22:13:23 +0200

    changed to wind speed

--- mendel-dashboard/src/app/map/map.component.ts
+++ mendel-dashboard/src/app/map/map.component.ts
@@ -25,21 +25,22 @@ export class MapComponent implements OnInit {
   private map?: L.Map;
 
   private layerControl?: L.Control.Layers;
-  private temperatureLayer?: L.Layer;
+  private windSpeedLayer?: L.Layer;
   private humidityLayer?: L.Layer;
 
   data: StationData[] = [];
 
-  temperatureText: string = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-half" viewBox="0 0 16 16">\
-            <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V6.5a.5.5 0 0 1 1 0v4.585a1.5 1.5 0 0 1 1 1.415"/>\
-            <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1"/>\
-         </svg>';
+  windSpeedText: string = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">\
+                            <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5"/>\
+                          </svg>';
 
-  humidityText: string = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-drizzle" viewBox="0 0 16 16">\
-          <path d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973M8.5 2a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4 4 0 0 1 8.5 2"/>\
-        </svg>';
+  humidityText: string = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moisture" viewBox="0 0 16 16">\
+                            <path d="M13.5 0a.5.5 0 0 0 0 1H15v2.75h-.51.5.5 0 0 0 0 1h.5V7.5h-1.5a.5.5 0 0 0 0 1H15v2.75h-.5a.5.5 0 0 0 0 1h.5V15h-1.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V.5a.5.5 0 0 0-.5-.5zM7 1.5l.364-.343a.5.5 0 0 0-.728 0l-.002.002-.006.007-.022.023-.08.088a29 29 0 0 0-1.274 1.517c-.769.983-1.714 2.325-2.385 3.727C2.368 7.564 2 8.682 2 9.733 2 12.614 4.212 15 7 15s5-2.386 5-5.267c0-1.05-.368-2.169-.867-3.212-.671-1.402-1.616-2.744-2.385-3.727a29 29 0 0 0-1.354-1.605l-.022-.023-.006-.007-.002-.001zm0 0-.364-.343zm-.016.766L7 2.247l.016.019c.24.274.572.667.944 1.144.611.781 1.32 1.776 1.901 2.827H4.14c.58-1.051 1.29-2.046 1.9-2.827.373-.477.706-.87.945-1.144zM3 9.733c0-.755.244-1.612.638-2.496h6.724c.395.884.638 1.741.638 2.496C11 12.117 9.182 14 7 14s-4-1.883-4-4.267"/>\
+                          </svg>';
 
-  selectedLayer: string = this.temperatureText;
+
+
+  selectedLayer: string = this.windSpeedText;
 
   currentTime: Date = new Date();
 
@@ -106,11 +107,11 @@ export class MapComponent implements OnInit {
       this.data = data;
 
       // Map the data to the format that heatLayer expects
-      const tempData = this.data.map(datum => [
+      const windSpeedData = this.data.map(datum => [
         datum.LAT,
         datum.LONG,
         // get average temperature
-        datum.DATA.reduce((acc, curr) => acc + parseFloat(curr.TEMP!), 0) / datum.DATA.length
+        datum.DATA.reduce((acc, curr) => acc + parseFloat(curr.WDSP!), 0) / datum.DATA.length
       ]);
 
       const humData = this.data.map(datum => [
@@ -121,11 +122,11 @@ export class MapComponent implements OnInit {
       ]);
 
       // create teamperature layer
-      if (this.temperatureLayer) {
-        this.map!.removeLayer(this.temperatureLayer);
+      if (this.windSpeedLayer) {
+        this.map!.removeLayer(this.windSpeedLayer);
       }
 
-      this.temperatureLayer = (L as any).idwLayer(tempData, {
+      this.windSpeedLayer = (L as any).idwLayer(windSpeedData, {
         opacity: 0.3,
         maxZoom: 18,
         cellSize: 5,
@@ -147,14 +148,14 @@ export class MapComponent implements OnInit {
       })
 
       var heatMaps = {
-        [this.temperatureText]: this.temperatureLayer,
+        [this.windSpeedText]: this.windSpeedLayer,
         [this.humidityText]: this.humidityLayer,
       };
 
       // Add the selected layer to the map
-      if (this.selectedLayer === this.temperatureText) {
+      if (this.selectedLayer === this.windSpeedText) {
         // cast as Layer to avoid type error
-        this.map!.addLayer(this.temperatureLayer as L.Layer);
+        this.map!.addLayer(this.windSpeedLayer as L.Layer);
       } else if (this.selectedLayer === this.humidityText) {
         // cast as Layer to avoid type error
         this.map!.addLayer(this.humidityLayer as L.Layer);
